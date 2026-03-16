6d3c680ecf38b9b5de87b8797eb241eca8d3479c
Hamid Shah <133199761+hamidanoo@users.noreply.github.com>
2026-03-16 17:01:59 +0500

    Updated

--- src/components/screens/Screen2.jsx
+++ src/components/screens/Screen2.jsx
@@ -23,7 +23,7 @@ export default function Screen2({ onNext }) {
 
       <div className="space-y-3 relative z-10">
         <h1 className="text-[2rem] font-black tracking-tight text-transparent bg-clip-text bg-linear-to-b from-rose-100 to-rose-400 drop-shadow-[0_0px_15px_rgba(244,113,133,0.4)] leading-tight relative z-10">
-          You can't go too far from me <span className='text-white'>😌</span>
+          Tum muj se dur nahi ja skti samji <span className='text-white'>😌</span>
         </h1>
         <p className="text-pink-200/70 font-medium text-lg">
           Want to know why?
@@ -37,7 +37,7 @@ export default function Screen2({ onNext }) {
         className="w-full mt-2 px-8 py-4 bg-linear-to-r from-[#fb7185] to-[#e11d48] rounded-4xl font-semibold text-white shadow-[0_0_25px_rgba(225,29,72,0.4)] transition-all flex items-center justify-center gap-2 text-lg group relative overflow-hidden will-change-transform"
       >
         <span className="relative z-10 flex items-center gap-2">
-          Let me show you <MoveRight className="w-5 h-5" />
+          Click Here 🤍 <MoveRight className="w-5 h-5" />
         </span>
       </motion.button>
     </motion.div>
