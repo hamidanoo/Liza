5ee3f1313dae777374241cb964b918a1611d9b1a
Hamid Shah <133199761+hamidanoo@users.noreply.github.com>
2026-03-16 17:04:22 +0500

    Updated

--- src/components/screens/FinalScreen.jsx
+++ src/components/screens/FinalScreen.jsx
@@ -35,7 +35,7 @@ export default function FinalScreen() {
         Forever Mine ❤️
       </h1>
       <p className="text-pink-200/80 font-medium text-lg">
-        I love you so much ❤️
+        I love you so much Alia ❤️
       </p>
     </motion.div>
   </motion.div>
